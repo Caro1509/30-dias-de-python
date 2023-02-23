a8a95bbd2997f0ad318c7f25bc13cdde02ddcdf0
carolinacolombo001 <carolinacolombo001@gmail.com>
2023-02-23 09:17:59 +0100

    dia4 f

--- dia_4/dia4.py
+++ dia_4/dia4.py
@@ -46,3 +46,46 @@ print(company.swapcase()) # "cODING fOR aLL"
 
 company = "Coding For All"
 print(company.slice(word1)) # "For All"
+
+
+print(multiline_string2.count('coding'))
+print(multiline_string2.replace('coding' , 'python'))
+print(multiline_string2.replace('all' , 'everyone'))
+print(multiline_string2.split())
+
+multiline_string3 = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+print(multiline_string3.split(', '))
+
+print(multiline_string2.index)
+print(multiline_string2.rindex)
+print(multiline_string2.index(10))
+
+multiline_string4 = """Python For Everyone"""
+print(multiline_string4)
+
+palabra1 = 'Phyton'
+print(len(palabra1))
+palabra2 = 'For'
+print(len(palabra2))
+palabra3 = 'Everyone'
+print(len(palabra3))
+
+primera_letra = palabra1[0]
+print(primera_letra) #P
+segunda_letra = palabra2[0]
+print(segunda_letra) #F
+tercera_letra = palabra3[0]
+print(tercera_letra) #E
+
+print(f"{primera_letra}{segunda_letra}{tercera_letra}")
+
+palabra4 = 'Coding'
+print(len(palabra4))
+palabra5 = 'For'
+print(len(palabra5))
+palabra6 = 'All'
+print(len(palabra5))
+
+
+
+
