4ede979adc40d13a185dbe214b5f878e3d56540a
carolinacolombo001 <carolinacolombo001@gmail.com>
2023-03-10 11:11:23 +0100

    empezando level 2 de dia 9

--- dia_9/conditionals.py
+++ dia_9/conditionals.py
@@ -1,6 +1,21 @@
-age = 17
+my_age = 17
+your_age = 30
 
-if age > 18:
+if my_age > 18:
     print('age puedes conducir')
-elif age < 18:
+elif my_age < 18:
     print('age no puedes conducir')
+    diference = 18 - my_age
+    print('necesitas(diference)para poder conducir')
+diference = your_age - my_age
+print(diference)
+
+print(f'i am {your_age - my_age} older than you') if your_age > my_age else print(f'you are {my_age - your_age} older than me')
+
+number_one = 4
+number_two = 3
+if number_one > number_two:
+    print('numero uno es mejor que numero dos')
+elif number_one < number_two:
+    print('numero uno es menor que numero dos')
+
